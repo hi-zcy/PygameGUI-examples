41cde7261db4ef3ae0e1267a5de1d8f9f72b21db
Zcyyyyyyyyyy <160196654+Zcyyyyyyyyyy@users.noreply.github.com>
2024-02-18 16:21:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # for i in range(10):
 #     print('Happy new year!')
 
-import pygame
+#import pygame
 import sys
-import src.text_edit
-import src.complexd-chooser-box.checking1
-import src.complexd-chooser-box.checking2
-import src.enter-frame.checking
-import src.push-button.checking
-import src.scorll-bar.checking
+#import src.text_edit
+#import src.complexd-chooser-box.checking1
+#import src.complexd-chooser-box.checking2
+#import src.enter-frame.checking
+#import src.push-button.checking
+#import src.scorll-bar.checking
 
 sys.exit(0)
